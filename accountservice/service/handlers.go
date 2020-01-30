002b3bf31f1d4364c73524f8dd9bcd426e3d7299
kimjg <kimjg@markany.com>
2020-01-30 18:00:57 +0900

    health test  가능하게 추가 curl localhost:6767/testability/healthy/false

--- accountservice/service/handlers.go
+++ accountservice/service/handlers.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -32,7 +33,7 @@ func GetAccount(w http.ResponseWriter, r *http.Request) {
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	// Since we're here, we already know that HTTP service is up. Let's just check the state of the boltdb connection
 	dbUp := DBClient.Check()
-	if dbUp {
+	if dbUp && isHealthy {
 		data, _ := json.Marshal(healthCheckResponse{Status: "UP"})
 		writeJsonResponse(w, http.StatusOK, data)
 	} else {
@@ -50,4 +51,19 @@ func writeJsonResponse(w http.ResponseWriter, status int, data []byte) {
 
 type healthCheckResponse struct {
 	Status string `json:"status"`
+}
+
+var isHealthy = true // NEW
+func SetHealthyState(w http.ResponseWriter, r *http.Request) {
+	// Read the 'state' path parameter from the mux map and convert to a bool
+	var state, err = strconv.ParseBool(mux.Vars(r)["state"])
+	// If we couldn't parse the state param, return a HTTP 400
+	if err != nil {
+		fmt.Println("Invalid request to SetHealthyState, allowed values are true or false")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// Otherwise, mutate the package scoped "isHealthy" variable.
+	isHealthy = state
+	w.WriteHeader(http.StatusOK)
 }
