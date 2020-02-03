4f2e43685ad5442fb1cfd75613c2a52dcd42cf8e
kimjg <kimjg@markany.com>
2020-02-03 09:42:24 +0900

    loading the configuration, using viper,

--- accountservice/go.mod
+++ accountservice/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/gorilla/mux v1.7.3
 	github.com/smartystreets/goconvey v1.6.4
+	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9 // indirect
 	gopkg.in/h2non/gock.v1 v1.0.15
--- accountservice/main.go
+++ accountservice/main.go
@@ -1,18 +1,43 @@
 package main // import "github.com/callistaenterprise/goblog/accountservice"
 
 import (
+	"flag"
 	"fmt"
 
+	"github.com/callistaenterprise/goblog/accountservice/config"
 	"github.com/callistaenterprise/goblog/accountservice/dbclient"
 	"github.com/callistaenterprise/goblog/accountservice/service"
+
+	"github.com/spf13/viper"
 )
 
 var appName = "accountservice"
 
+// Init function, runs before main()
+func init() {
+	// Read command line flags
+	profile := flag.String("profile", "test", "Environment profile, something similar to spring profiles")
+	configServerUrl := flag.String("configServerUrl", "http://configserver:8888", "Address to config server")
+	configBranch := flag.String("configBranch", "master", "git branch to fetch configuration from")
+	flag.Parse()
+
+	// Pass the flag values into viper.
+	viper.Set("profile", *profile)
+	viper.Set("configServerUrl", *configServerUrl)
+	viper.Set("configBranch", *configBranch)
+}
+
 func main() {
 	fmt.Printf("Starting %v\n", appName)
+
+	config.LoadConfigurationFromBranch(
+		viper.GetString("configServerUrl"),
+		appName,
+		viper.GetString("profile"),
+		viper.GetString("configBranch"))
+
 	initializeBoltClient() // NEW
-	service.StartWebServer("6767")
+	service.StartWebServer(viper.GetString("server_port"))
 }
 
 // Creates instance and calls the OpenBoltDb and Seed funcs
